421589f0a1f194ba6540c5e98aa4b69f51349a41
Franck Lavisse <lavisse.franck@gmail.com>
2021-10-30 17:22:52 +0200

    config for producer

--- producer.rb
+++ producer.rb
@@ -1,7 +1,13 @@
 require 'ruby-kafka'
 
-kafka = Kafka.new(["localhost:9092"], client_id: "my_app")
-producer = kafka.async_producer(delivery_interval: 1)
+kafka = Kafka.new(["localhost:9092"])
+
+producer = kafka.async_producer(
+    delivery_interval: 1, # deliver each second
+    required_acks: :all, # already set to :all by default, but I wanted to write a config example
+    max_buffer_size: 100, # max 100 messages 
+    max_buffer_bytesize: 100_000 # max 10M of data
+)
 
 producer.produce("hello world", topic: "first_topic")
 producer.produce("second message", topic: "first_topic")
@@ -9,4 +15,4 @@ producer.produce("byebye", topic: "first_topic")
 
 producer.deliver_messages
 
-producer.shutdown
+at_exit { producer.shutdown }
