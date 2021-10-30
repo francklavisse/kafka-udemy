ef575ca866c68197ceec19ecd5af7edd75ed42b7
Franck Lavisse <lavisse.franck@gmail.com>
2021-10-30 17:37:23 +0200

    config for consumer

--- consumer.rb
+++ consumer.rb
@@ -2,9 +2,16 @@ require 'ruby-kafka'
 
 kafka = Kafka.new(["localhost:9092"])
 
-consumer = kafka.consumer(group_id: "group_1")
+consumer = kafka.consumer(
+    group_id: "group_a",
+    offset_commit_interval: 2, # commit each 2 seconds
+    offset_commit_threshold: 5, # commit offset each 5 messages
+)
 
-consumer.subscribe("first_topic")
+consumer.subscribe(
+    "first_topic",
+    start_from_beginning: true    
+)
 
 trap("TERM") { consumer.stop }
 
