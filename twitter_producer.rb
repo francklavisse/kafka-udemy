10457670d3e6f51e956e7d6de1c2dd2e1c681e5f
Franck Lavisse <lavisse.franck@gmail.com>
2021-10-31 16:54:22 +0100

    improve producer configuration

--- twitter_producer.rb
+++ twitter_producer.rb
@@ -12,18 +12,18 @@ end
 kafka = Kafka.new(["localhost:9092"])
 
 producer = kafka.async_producer(
-    delivery_interval: 1, # deliver each second
-    required_acks: :all, # already set to :all by default, but I wanted to write a config example
-    max_buffer_size: 100, # max 100 messages 
-    max_buffer_bytesize: 100_000, # max 10M of data
     compression_codec: :lz4,
     compression_threshold: 5,
+    delivery_interval: 1,
+    delivery_threshold: 10,
+    max_buffer_bytesize: 1_000,
+    max_buffer_size: 100,
+    required_acks: :all,
 )
 
 topics = ["bitcoin", "eth", "btc"]
 client.filter(track: topics.join(",")) do |object|
     if object.is_a?(Twitter::Tweet)
-        puts object.text 
         producer.produce(object.text, topic: "twitter_tweets")
     end
 end
